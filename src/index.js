dc812fd3706d34b06b5da281678fe7022b20fbce
willparr <52459758+willparr@users.noreply.github.com>
2021-02-01 09:19:44 -0600

    Feature | Adding Light Functionality and Hue Client Class (#5)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch")
 const {getColor, getRandomColor} = require("./colors")
+const {sleep} = require("./utils")
 
 class Hue {
     constructor(ip, username) {
@@ -39,38 +40,88 @@ class Hue {
         this.updateLight(id, {on: true})
     }
 
+    turnOnLights = ids => {
+        ids.forEach(id => this.turnOnLight(id))
+    }
+
+    turnOnAllLights = async () => {
+        const lights = await this.readLights()
+
+        lights.forEach(light => {
+            this.turnOnLight(light.id)
+        })
+    }
+
     turnOffLight = id => {
         this.updateLight(id, {on: false})
+    }
+
+    turnOffLights = ids => {
+        ids.forEach(id => this.turnOffLight(id))
+    }
+
+    turnOffAllLights = async () => {
+        const lights = await this.readLights()
+
+        lights.forEach(light => {
+            this.turnOffLight(light.id)
+        })
+    }
+
+    blinkLight = async (id, interval = 500, count = 1) => {
+        const light = await this.readLight(id)
+
+        for (let index = 0; index < count; index++) {
+            this.updateLight(id, {on: !light.state.on})
+            await sleep(interval)
+
+            this.updateLight(id, {on: light.state.on})
+            await sleep(interval)
+        }
+    }
+
+    blinkLights = (ids, interval = 500, count = 1) => {
+        ids.forEach(id => this.blinkLight(id, interval, count))
     }
 
     setBrightness = (id, brightness) => {
         this.updateLight(id, {bri: brightness})
     }
 
+    setBrightnesses = (ids, brightness) => {
+        ids.forEach(id => this.setBrightness(id, brightness))
+    }
+
     setColor = (id, color) => {
         if (color === "random") {
-            this.setRandomColor(id)
-        } else {
-            this.updateLight(id, {xy: getColor(color)})
+            const randomColor = this.setRandomColor(id)
+            return randomColor
         }
+
+        this.updateLight(id, {xy: getColor(color)})
+        return color
     }
 
     setColors = (ids, color) => {
         if (color === "random") {
-            this.setRandomColors(ids)
-        } else {
-            ids.forEach(id => this.setColor(id, color))
+            const randomColor = this.setRandomColors(ids)
+            return randomColor
         }
+
+        ids.forEach(id => this.setColor(id, color))
+        return color
     }
 
     setRandomColor = id => {
         const color = getRandomColor()
         this.setColor(id, color)
+        return color
     }
 
     setRandomColors = ids => {
         const color = getRandomColor()
         this.setColors(ids, color)
+        return color
     }
 }
 
--- src/utils.js
+++ src/utils.js
@@ -6,4 +6,8 @@ const randomInteger = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-module.exports = {randomDecimal, randomInteger}
+const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+module.exports = {randomDecimal, randomInteger, sleep}
