a8fa0b2a4b553429089061186e2774499983ba6d
Brad Garropy <bradgarropy@gmail.com>
2021-01-31 22:38:46 -0600

    return promise from sleep.

--- src/utils.js
+++ src/utils.js
@@ -6,8 +6,8 @@ const randomInteger = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-const sleep = async ms => {
-    await new Promise(resolve => setTimeout(resolve, ms))
+const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 module.exports = {randomDecimal, randomInteger, sleep}
